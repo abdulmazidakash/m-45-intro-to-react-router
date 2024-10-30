604429e5224f7ca2e6f32c76a57968c82c373ccc
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-30 07:09:09 +0600

    45-2

--- src/main.jsx
+++ src/main.jsx
@@ -9,26 +9,33 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello from react router</div>
-  },
-  {
-    path: '/about',
-    element: <div>I am in the about page</div>
-
-  },
-  {
-    path: '/contact',
-    element: <div>Call me right now!!</div>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
+  
   }
 ])
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
 
